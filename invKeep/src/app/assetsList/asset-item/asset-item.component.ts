b26acee7366de1ff2a1ad9d28f494eecfd72034d
Piotr Paterek <ptrptrk@gmail.com>
2021-04-05 22:10:41 +0200

    v0.1 - Preliminar possibility of asset list creation

---
+++ invKeep/src/app/assetsList/asset-item/asset-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-item',
+  templateUrl: './asset-item.component.html',
+  styleUrls: ['./asset-item.component.scss']
+})
+export class AssetItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
