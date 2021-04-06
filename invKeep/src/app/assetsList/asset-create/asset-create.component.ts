7ec20890326881bbf5b928e9db8fe90ac4a8afc0
Piotr Paterek <ptrptrk@gmail.com>
2021-04-06 11:04:56 +0200

    v0.01.02 - remove of assets and clear button

--- invKeep/src/app/assetsList/asset-create/asset-create.component.ts
+++ invKeep/src/app/assetsList/asset-create/asset-create.component.ts
@@ -16,16 +16,37 @@ export class AssetCreateComponent {
     }
   returnedString: string;
 
+  onAssetSave(nameF: HTMLInputElement, symbol: HTMLInputElement, amount: HTMLInputElement, basicPrice: HTMLInputElement) {
 
-  onAssetSave(nameF: HTMLInputElement, symbol: HTMLInputElement, amount: HTMLInputElement, basicPrice: HTMLInputElement){
     this.placeholderAsset = {
       nameFull: nameF.value.toString(),
       symbol: symbol.value.toString(),
       amount: parseFloat(amount.value),
       basicPrice: parseFloat(basicPrice.value)
     }
-    let nextItem = document.createElement("DIV");
+
+    let nextItem = document.createElement(`DIV`);
+    let nextItemButton = document.createElement(`BUTTON`);
+
+    nextItemButton.setAttribute(`value`, `Remove`);
+    nextItemButton.setAttribute(`class`, `remove`);
+    nextItemButton.setAttribute(`id`, `${this.placeholderAsset.nameFull}`);
+    nextItemButton.innerText = `Remove!`;
+    nextItemButton.addEventListener('click', (e) => {
+      this.removeItem(e);//your typescript function
+    });
+
     nextItem.innerHTML = `Name: ${this.placeholderAsset.nameFull}<br/>Symbol: ${this.placeholderAsset.symbol}<br/>Amount: ${this.placeholderAsset.amount}<br/>Basic Price: ${this.placeholderAsset.basicPrice}<br/> - <br/>`;
+    nextItem.setAttribute(`id`, `${this.placeholderAsset.symbol}`);
+    nextItem.setAttribute(`name`, `${this.placeholderAsset.nameFull}`);
+    nextItem.appendChild(nextItemButton);
+
     return  document.getElementById('output').appendChild(nextItem);
   };
+
+  removeItem(e){
+    var removeAsset = e.target.parentNode;
+    removeAsset.remove();
+
+  }
 }
