63e8005d12a63e65672b4194b78406dbddea90f4
Piotr Paterek <ptrptrk@gmail.com>
2021-04-16 09:01:07 +0200

    v0.01.11 - add asset service with getter and setter

---
+++ invKeep/src/app/assetComponents/asset-list/assets.service.ts
@@ -0,0 +1,17 @@
+import {assetRecord} from "../../shared/shared";
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class AssetsService{
+
+  private assetsArray: assetRecord[] = [];
+
+  addAssets(assetItem: assetRecord){
+    this.assetsArray.push(assetItem);
+  }
+
+  getAssets(){
+    return [...this.assetsArray];
+  }
+
+}
