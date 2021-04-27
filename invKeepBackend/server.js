71c15891c93d2222ca94d5c79a4d9f09bdd08e89
Piotr Paterek <ptrptrk@gmail.com>
2021-04-27 22:49:51 +0200

    v0.02.04 - add more complex server basics

--- invKeepBackend/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app.js');
-
-const port = process.env.PORT || 3000;
-
-app.set('port', port);
-const server = http.createServer(app);
-
-server.listen(port);
