c490489a3d2173e85dff3aee134b67bdca591c97
Piotr Paterek <ptrptrk@gmail.com>
2021-04-28 22:51:29 +0200

    v0.02.03 - dummy resource return

--- invKeepBackend/app.js
+++ invKeepBackend/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Second middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.end('First response from express!');
+app.use('/api/resources', (req, res, next) => {
+    const assets = [
+        {
+            assetName: 'Comerica',
+            assetSymbol: 'CMA',
+            amount: 12,
+            buyPrice: 58.56,
+            currency: '€',
+            purchaseDate: '12/11/2019'
+        },
+        {
+            assetName: 'Nio',
+            assetSymbol: 'NIO',
+            amount: 325,
+            buyPrice: 12.13,
+            currency: '$',
+            purchaseDate: '22/12/2020'
+        }
+    ]
+    res.json({
+        message: 'Asset list returned successfully!',
+        payload: assets
+    });
 });
 
 module.exports = app;
