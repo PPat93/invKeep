5358656f4a8285f89bbe3de1abfe4b4ba43836a1
Piotr Paterek <ptrptrk@gmail.com>
2021-05-02 20:56:50 +0200

    v0.02.05 - add request sending to server through asset service, display dummy values from api/assets, add id to asset obj, i

--- invKeepFrontend/src/app/app.module.ts
+++ invKeepFrontend/src/app/app.module.ts
@@ -2,15 +2,16 @@ import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http'
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatExpansionModule} from '@angular/material/expansion';
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from "@angular/material/input";
-import {MatNativeDateModule} from "@angular/material/core";
-import {MatSelectModule} from "@angular/material/select"
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select'
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import {AppComponent} from './app.component';
@@ -31,6 +32,7 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     MatButtonModule,
     MatCardModule,
     MatDatepickerModule,
