f46ba8eb31d140dc84ff1ddf7dbe6e737922d500
Piotr Paterek <ptrptrk@gmail.com>
2021-05-04 16:10:31 +0200

    v0.02.06 - remove dummy posts from server

--- invKeepBackend/app.js
+++ invKeepBackend/app.js
@@ -26,26 +26,6 @@ app.post('/api/assets',(req, res, next) => {
 })
 
 app.get('/api/assets', (req, res, next) => {
-    // const assets = [
-    //     {
-    //         id: 1,
-    //         assetName: 'Comerica',
-    //         assetSymbol: 'CMA',
-    //         amount: 12,
-    //         buyPrice: 58.56,
-    //         currency: '€',
-    //         purchaseDate: '12/11/2019'
-    //     },
-    //     {
-    //         id: 2,
-    //         assetName: 'Nio',
-    //         assetSymbol: 'NIO',
-    //         amount: 325,
-    //         buyPrice: 12.13,
-    //         currency: '$',
-    //         purchaseDate: '22/12/2020'
-    //     }
-    // ]
 
     res.status(200).json({
         message: 'Asset list returned successfully!',
