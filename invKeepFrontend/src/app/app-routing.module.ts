7bdd91a5839fd833079d207c1d0e2fab8031bb27
Piotr Paterek <ptrptrk@gmail.com>
2021-05-10 22:16:18 +0200

    v0.03.04 - link router, changecomponent displayment, preliminary routerLinks

--- invKeepFrontend/src/app/app-routing.module.ts
+++ invKeepFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import {NgModule} from "@angular/core";
-import {RouterModule} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
+
+import {AssetListComponent} from "./assetComponents/asset-list/asset-list.component";
+import {AssetCreateComponent} from "./assetComponents/asset-create/asset-create.component";
+
+const routes: Routes = [
+  {path: '', component: AssetListComponent},
+  {path: 'create', component: AssetCreateComponent}
+]
 
 @NgModule({
-
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
