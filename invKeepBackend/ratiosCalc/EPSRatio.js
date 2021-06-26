eb8530f407d68a4da9410459b75c9ee48a3e2c60
Piotr Paterek <ptrptrk@gmail.com>
2021-06-26 23:25:06 +0200

    v0.05.02 - create structure, add class for EPS calcualations

---
+++ invKeepBackend/ratiosCalc/EPSRatio.js
@@ -0,0 +1,13 @@
+
+class EPSRatio {
+
+    val_EPSRatio;
+
+    constructor(EPSRatio) {
+        this.val_EPSRatio = EPSRatio;
+    }
+
+    calcProfitability() {
+        // val_EPSRatio =
+    }
+}
