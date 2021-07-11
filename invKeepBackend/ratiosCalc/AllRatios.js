e0ac6b33427a88c7c587b741fd7a45f4abe05de0
Piotr Paterek <ptrptrk@gmail.com>
2021-07-11 22:30:44 +0200

    v0.05.05 - add object creation in AllRatios

--- invKeepBackend/ratiosCalc/AllRatios.js
+++ invKeepBackend/ratiosCalc/AllRatios.js
@@ -1,6 +1,7 @@
 const sharedJS = require ('../../invKeepFrontend/src/app/shared/sharedJS');
 const EPSRatio = require ('./ratiosClasses/EPSRatio');
 const PERatio = require ('./ratiosClasses/PERatio');
+const PEGRatio = require('./ratiosClasses/PEGRatio');
 
 module.exports = class AllRatios {
 
@@ -8,9 +9,10 @@ module.exports = class AllRatios {
          // this.createEPSClass(detailedRatiosArray);
     }
 
-   createRatios(detailedRatiosArray) {
+    createRatiosObjects(detailedRatiosArray) {
         let objEPSRatio = new EPSRatio((sharedJS.searchObject(detailedRatiosArray, sharedJS.RatiosNames.eps_ratio)).valueNum);
         let objPERatio = new PERatio((sharedJS.searchObject(detailedRatiosArray, sharedJS.RatiosNames.pe_ratio)).valueNum);
-        return [objEPSRatio.determineProfitability(objEPSRatio.final_value), objPERatio.determineProfitability(objPERatio.final_value)];
+        let objPEGRatio = new PEGRatio((sharedJS.searchObject(detailedRatiosArray, sharedJS.RatiosNames.peg_ratio)).valueNum);
+
     }
 }
