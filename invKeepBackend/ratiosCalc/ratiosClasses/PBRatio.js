45678ef115716770473ce2a9687782f78aec1163
Piotr Paterek <ptrptrk@gmail.com>
2021-07-12 23:11:35 +0200

    v0.05.07 - create pb ratio file

---
+++ invKeepBackend/ratiosCalc/ratiosClasses/PBRatio.js
@@ -0,0 +1,6 @@
+const SharedJS = require('../../../invKeepFrontend/src/app/shared/sharedJS');
+const BasicRatio = require('../BasicRatio')
+
+module.exports = class PBRatio extends BasicRatio {
+
+}
