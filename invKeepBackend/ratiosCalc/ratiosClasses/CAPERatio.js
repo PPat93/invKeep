047d3aae8d7e8307098cf8eb7f84e9d92c25be59
Piotr Paterek <ptrptrk@gmail.com>
2021-07-12 23:01:06 +0200

    v0.05.06 - shared add rating object or js, update ratings across ratios classes, add imports for this object, add more info about cape ratio

--- invKeepBackend/ratiosCalc/ratiosClasses/CAPERatio.js
+++ invKeepBackend/ratiosCalc/ratiosClasses/CAPERatio.js
@@ -1,58 +1,62 @@
 const BasicRatio = require('../BasicRatio');
+const SharedJS = require('../../../invKeepFrontend/src/app/shared/sharedJS');
 
 module.exports = class CAPERatio extends BasicRatio {
 
-    constructor(EPSRatio = 0) {
+    constructor(CAPERatio = 0) {
         super();
         this.ratioName  = `CAPE Ratio`;
-        this.coAnalysis = [``];
+        this.coAnalysis = [`-`];
         this.description = `Cyclically-Adjusted Price-to-Earnings Ratio is variation on P/E Ratio (Shiller P/E). Allows
         to asses company's capabilities of making profits during the economic cycle. Considers economy fluctuations - 
         recessions/expansions and makes easier to analyze company without them. The lower, the better - however does 
         not take into account accounting changes.`;
         this.additionalDetails = [
-            ``,
-            ``,
+            `Adjusted to economic cycles.`,
+            `Shows if stock is over or under valued.`,
             `The higher value the worse .`,
+            `Analyzes long term company results.`,
             `Analyze with: ${this.coAnalysis}`
         ];
-        this.final_value = EPSRatio;
+        this.final_value = CAPERatio;
         this.onScaleRating = 0;
     }
 
 
     // calculate() { // future ratio more precise analysis method
-    //     let
+    //     let stockPrice, 10yearInflationAdjusted;
     //     // here external def of above values for calculation
-    //     return this.final_value =
+    //     return this.final_value = stockPrice / 10yearInflationAdjusted;
     // }
 
-    determineProfitability(EPSRatio) {
+    determineProfitability(CAPERatio) {
         switch (true) {
-            case (EPSRatio < 0):
-                this.analysisSummary = [`${this.ratioName}`, `Really bad`, `Negative value indicates loss`, this.onScaleRating = 1];
+            case (0 < CAPERatio <= 10):
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.outstanding, `value between 0 and 10 indicates great 
+                possibilities, average return  after 10 - 15 years is 11.7%`, this.onScaleRating = 6];
                 break;
-            case (EPSRatio === 0):
-                this.analysisSummary = [`${this.ratioName}`, `Bad`, `0 value indicates no profits`, this.onScaleRating = 2];
+            case (10 < CAPERatio < 15):
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.rather_good, `value between 10 and 15 indicates good 
+                possibilities, average return  after 10 - 15 years is 8.7%`, this.onScaleRating = 5];
                 break;
-            case (0 < EPSRatio < 13):
-                this.analysisSummary = [`${this.ratioName}`, `Rather bad`, `Significantly below average (counted as 
-                average of S&P500 from last 20 years)`, this.onScaleRating = 3];
+            case (15 <= CAPERatio < 20):
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.ok, `value between 15 and 20 indicates quite OK 
+                possibilities, average return  after 10 - 15 years is 7.2%`, this.onScaleRating = 4];
                 break;
-            case (13 <= EPSRatio < 26):
-                this.analysisSummary = [`${this.ratioName}`, `Not really good`, `Slightly below average (counted as 
-                average of S&P500 from last 20 years)`, this.onScaleRating = 4];
+            case (20 <= CAPERatio < 25):
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.neutral, `value between 20 and 25 indicates normal 
+                possibilities, average return  after 10 - 15 years is 5.7%`, this.onScaleRating = 3];
                 break;
-            case (26 <= EPSRatio < 39):
-                this.analysisSummary = [`${this.ratioName}`, `Rather good`, `Slightly above average (counted as average 
-                of S&P500 from last 20 years)`, this.onScaleRating] = 5;
+            case (25 <= CAPERatio < 30):
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.rather_bad, `value between 25 and 30 indicates bad 
+                possibilities, average return  after 10 - 15 years is 4.1%`, this.onScaleRating = 2];
                 break;
-            case (39 <= EPSRatio):
-                this.analysisSummary = [`${this.ratioName}`, `Outstanding`, `Significantly above average (counted as 
-                average of S&P500 from last 20 years)`, this.onScaleRating = 6];
+            case (30 <= CAPERatio):
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.terrible, `value between 25 and 30 indicates bad 
+                possibilities, average return  after 10 - 15 years is 0.5%`, this.onScaleRating = 1];
                 break;
             default:
-                this.analysisSummary = [`${this.ratioName}`, `Error`, `Data is out of boundaries - value: ${EPSRatio}`, this.onScaleRating];
+                this.analysisSummary = [`${this.ratioName}`, SharedJS.RatiosObject.error, `Data is out of boundaries - value: ${CAPERatio}`, this.onScaleRating];
         }
         return this.analysisSummary;
     }
--- invKeepFrontend/src/app/shared/sharedJS.js
+++ invKeepFrontend/src/app/shared/sharedJS.js
@@ -15,6 +15,17 @@ const RatiosNames = Object.freeze({
   ev_ebitda_ratio: `EV/EBITDA Ratio`
 });
 
+const RatiosObject = Object.freeze({
+  error: `Error`,
+  depends: `Depends`,
+  terrible: `Terrible`,
+  rather_bad: `Rather bad`,
+  neutral: `Neutral`,
+  ok: `OK`,
+  rather_good: `Rather good`,
+  outstanding: `Outstanding`
+});
+
 function searchObject(ratiosArray, searchedNameObj) {
   for (let a = 0; a < ratiosArray.length; a++) {
     if (ratiosArray[a].parameterName === searchedNameObj) {
@@ -25,4 +36,4 @@ function searchObject(ratiosArray, searchedNameObj) {
   }
 }
 
-module.exports = {RatiosNames, searchObject};
+module.exports = {RatiosNames, searchObject, RatiosObject};
