db05028778e0e5b866a88f62756fd20e126acdb2
Piotr Paterek <ptrptrk@gmail.com>
2021-10-31 00:09:34 +0100

    v0.07.01 - update plugins with appropriate code resolving config env files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,3 +3,20 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+
+const fs = require('fs-extra');
+const path = require('path');
+
+function getConfigurationByFile (file) {
+    const pathToConfigFile = path.resolve('./cypress/config', `${file}.json`);
+
+    return fs.readJson(pathToConfigFile)
+}
+
+// plugins file
+module.exports = (on, config) => {
+    // accept a configFile value or use local by default
+    const file = config.env.configFile || 'local';
+
+    return getConfigurationByFile(file)
+};
