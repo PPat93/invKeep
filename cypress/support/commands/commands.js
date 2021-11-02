2e3c950a11fd2d3102a6c1a6545e7528b74b3e2a
Piotr Paterek <ptrptrk@gmail.com>
2021-11-02 19:24:51 +0000

    v0.07.01 - complete addition of data-cy attributes for cypress

--- cypress/support/commands/commands.js
+++ cypress/support/commands/commands.js
@@ -24,4 +24,4 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import './commandsFE/mainPageCommands-fe'
+import './commandsFE/mainPageCommands-fe';
