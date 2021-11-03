deabcda18968dfa0daa706dedd3af49141ead63a
Piotr Paterek <ptrptrk@gmail.com>
2021-11-03 21:18:13 +0000

    v0.07.02 - add create page test displayemnt and slight update of main page test

--- cypress/integration/functional/pagesDisplayments.spec.ts
+++ cypress/integration/functional/pagesDisplayments.spec.ts
@@ -1,5 +1,7 @@
 import MainPageConsts from "../../support/pageObjectModel/Utils/MainPageConsts";
 import MainPage from "../../support/pageObjectModel/pageObjects/MainPage";
+import Utils from "../../support/pageObjectModel/Utils/Utils";
+import CreatePageConsts from "../../support/pageObjectModel/Utils/CreatePageConsts";
 
 describe(`Page displayments after direct access from URL`, () => {
 
@@ -12,5 +14,29 @@ describe(`Page displayments after direct access from URL`, () => {
         cy.getDataCyElement(MainPageConsts.assetList)
             .should(`be.visible`)
             .and(`contain.text`, MainPageConsts.assetListHeader);
+        cy.getDataCyElement(MainPageConsts.appName).then(logo => {
+            cy.wrap(logo)
+                .should(`be.visible`);
+            expect(logo.text()).to.eq(`invKeep`);
+        })
     })
+
+    it(`Create Page displayment`, () => {
+
+        //  Arrange
+        MainPage.visitPage(MainPageConsts.mainPageUrl);
+
+        //  Act
+        cy.getDataCyElement(Utils.createAssetBtn)
+            .click();
+
+        //  Assert    
+        cy.url()
+            .should(`contain`, CreatePageConsts.createPageUrl);
+        cy.getDataCyElement(CreatePageConsts.createAssetForm)
+            .should(`be.visible`)
+            .and(`contain.text`, CreatePageConsts.createAssetFormHeader);
+    })
+
+    // TODO details page and edit page
 })
