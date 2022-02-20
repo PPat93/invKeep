d1fe959629fff16c89ff8924abea57f564ddd7ab
Piotr Paterek <ptrptrk@gmail.com>
2022-02-20 00:40:57 +0000

    v0.07.08 - add test covering rest of asset data displayment on edit asset form

--- invKeepTests/cypress/integration/functional/pagesDisplayments.spec.ts
+++ invKeepTests/cypress/integration/functional/pagesDisplayments.spec.ts
@@ -258,12 +258,15 @@ describe(`Visibility of Detailed Page elements`, () => {
     })
 })
 
-describe.only(`Displayment of asset data on asset edition`, () => {
-    
+describe(`Displayment of asset data on asset edition`, () => {
+
     let assetName = `TestAsset${Date.now()}`;
 
     let assetFullName: string[] = [`TestAsset`, `TestAsset-`, `TestAsset.`];
-    let assetSymbol: string[] = [`Sym`, `Sym,m`, `Sym.m`,`Sym_m`,`Sym(m`,`Sym)m`,`Sym-m`,];
+    let assetSymbol: string[] = [`Sym`, `Sym,m`, `Sym.m`, `Sym_m`, `Sym(m`, `Sym)m`, `Sym-m`];
+    let assetPricePerUnit: number[] = [123, 12.2, 12.23];
+    let assetCurrency: AssetCurrency[] = [AssetCurrency.dollar, AssetCurrency.euro, AssetCurrency.pound, AssetCurrency.yen];
+    let assetPurchaseDate: string[][] = [[`12/12/2012`, `12/12/2012`], [`04.05.2010`, `4/5/2010`]];
 
     afterEach(`Teardown after each test`, () => {
         Cypress.env("assetItem").forEach(singleAsset => {
@@ -273,7 +276,7 @@ describe.only(`Displayment of asset data on asset edition`, () => {
 
     assetFullName.forEach(singleAssetName => {
 
-        it(`Asset data full name is shown correctly on its edition - ${singleAssetName} value`, () => {
+        it(`Asset data full name in edit form displayment - ${singleAssetName} value`, () => {
 
             //  Arrange
             cy.apiCreateAsset(singleAssetName, `editVis`, 10, 1.21, AssetCurrency.dollar).then(res => {
@@ -297,7 +300,7 @@ describe.only(`Displayment of asset data on asset edition`, () => {
 
     assetSymbol.forEach(singleSymbol => {
 
-        it(`Asset data symbol is shown correctly on its edition - ${singleSymbol} value`, () => {
+        it(`Asset data symbol in edit form displayment - ${singleSymbol} value`, () => {
 
             //  Arrange
             cy.apiCreateAsset(assetName, singleSymbol, 10, 1.21, AssetCurrency.dollar).then(res => {
@@ -315,6 +318,98 @@ describe.only(`Displayment of asset data on asset edition`, () => {
             cy.getDataCyElement(CreateEditPageConsts.symbol)
                 .invoke(`val`).then(text => {
                     expect(text).to.be.equal(singleSymbol.toUpperCase());
+                })
+        })
+    })
+
+    it(`Asset amount in edit form displayment - 123 value`, () => {
+
+        //  Arrange
+        cy.apiCreateAsset(assetName, `amounSym`, 123, 1.21, AssetCurrency.dollar).then(res => {
+            if (res.status === 201)
+                Cypress.env("assetItem").set(assetName, res.body.assetId);
+        });
+        cy.visit(Utils.mainPageUrl);
+
+        cy.getDataCyElement(MainPage.dataCyElementAsset(assetName))
+            .click();
+        cy.getDataCyElement(MainPage.dataCyElementEditBtn(assetName))
+            .click();
+
+        //  Act & Assert
+        cy.getDataCyElement(CreateEditPageConsts.amount)
+            .invoke(`val`).then(text => {
+                expect(text).to.be.equal(`123`);
+            })
+    })
+
+    assetPricePerUnit.forEach(singlePriceUnit => {
+
+        it(`Asset price in edit form displayment - ${singlePriceUnit} value`, () => {
+
+            //  Arrange
+            cy.apiCreateAsset(assetName, `amounSym`, 123, singlePriceUnit, AssetCurrency.dollar).then(res => {
+                if (res.status === 201)
+                    Cypress.env("assetItem").set(assetName, res.body.assetId);
+            });
+            cy.visit(Utils.mainPageUrl);
+
+            cy.getDataCyElement(MainPage.dataCyElementAsset(assetName))
+                .click();
+            cy.getDataCyElement(MainPage.dataCyElementEditBtn(assetName))
+                .click();
+
+            //  Act & Assert
+            cy.getDataCyElement(CreateEditPageConsts.price)
+                .invoke(`val`).then(text => {
+                    expect(text).to.be.equal(singlePriceUnit.toString());
+                })
+        })
+    })
+
+    assetCurrency.forEach(singleCurrency => {
+
+        it(`Asset currency in edit form displayment - ${singleCurrency} value`, () => {
+
+            //  Arrange
+            cy.apiCreateAsset(assetName, `amounSym`, 123, 12.2, singleCurrency).then(res => {
+                if (res.status === 201)
+                    Cypress.env("assetItem").set(assetName, res.body.assetId);
+            });
+            cy.visit(Utils.mainPageUrl);
+
+            cy.getDataCyElement(MainPage.dataCyElementAsset(assetName))
+                .click();
+            cy.getDataCyElement(MainPage.dataCyElementEditBtn(assetName))
+                .click();
+
+            //  Act & Assert
+            cy.getDataCyElement(CreateEditPageConsts.currency)
+                .find(`span`)
+                .should(`contain.text`, singleCurrency);
+        })
+    })
+
+    assetPurchaseDate.forEach(singleDate => {
+
+        it(`Asset currency in edit form displayment - ${singleDate[0]} value`, () => {
+
+            //  Arrange
+            cy.apiCreateAsset(assetName, `amounSym`, 123, 12.2, AssetCurrency.dollar, singleDate[0]).then(res => {
+                if (res.status === 201)
+                    Cypress.env("assetItem").set(assetName, res.body.assetId);
+            });
+            cy.visit(Utils.mainPageUrl);
+
+            cy.getDataCyElement(MainPage.dataCyElementAsset(assetName))
+                .click();
+            cy.getDataCyElement(MainPage.dataCyElementEditBtn(assetName))
+                .click();
+
+            //  Act & Assert
+            cy.getDataCyElement(CreateEditPageConsts.purchaseDate)
+                .invoke(`val`).then(text => {
+                    expect(text).to.be.equal(singleDate[1]);
                 })
         })
     })
