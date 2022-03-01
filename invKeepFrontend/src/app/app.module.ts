64ded57f9ac199d6aee6a8972fb0ebdac2c18d4c
Piotr Paterek <ptrptrk@gmail.com>
2022-03-01 21:36:34 +0000

    Revert "v0.07.11 - import dialog component"
    This reverts commit b33d8220cdf1a2871e66d4804a70eefd04ce1baf.

--- invKeepFrontend/src/app/app.module.ts
+++ invKeepFrontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http'
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialog } from "@angular/material/dialog";
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
@@ -50,7 +49,6 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     MatButtonModule,
     MatCardModule,
     MatDatepickerModule,
-    MatDialog,
     MatDividerModule,
     MatExpansionModule,
     MatIconModule,
