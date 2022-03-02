1a42c8546cd09ad77ec39b77a7df8db5854d872a
Piotr Paterek <ptrptrk@gmail.com>
2022-03-02 20:26:19 +0000

    v0.07.11 - add a skeleton of  additional details dialog component

---
+++ invKeepFrontend/src/app/components/asset-details-dialog/asset-details-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: 'asset-details-dialog',
+templateUrl: 'asset-details-dialog.component.html',
+styles: ['asset-details-dialog.componeent.scss']
+})
+
+export class AssetDetailsDialogComponent{
+    
+}
