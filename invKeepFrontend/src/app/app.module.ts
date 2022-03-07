f83f6c951f0cd0730291d9466f7f366198c25dba
Piotr Paterek <ptrptrk@gmail.com>
2022-03-07 22:30:45 +0000

    v0.07.11 - update non-existent dialog component

--- invKeepFrontend/src/app/app.module.ts
+++ invKeepFrontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialog } from "@angular/material/dialog";
+import { MatDialogModule  } from "@angular/material/dialog";
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
@@ -50,7 +50,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     MatButtonModule,
     MatCardModule,
     MatDatepickerModule,
-    MatDialog,
+    MatDialogModule ,
     MatDividerModule,
     MatExpansionModule,
     MatIconModule,
