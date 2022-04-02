b6ff7828283e5c32851a515f7471c37aab6805e1
Piotr Paterek <ptrptrk@gmail.com>
2022-04-02 01:15:11 +0100

    v0.07.11 - adjust data get from backend for appropriate asset, get url incorporaated asset id, test backend data displayment in dialog

--- invKeepFrontend/src/app/components/ratio-details-dialog/ratio-details-dialog.component.ts
+++ invKeepFrontend/src/app/components/ratio-details-dialog/ratio-details-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from "@angular/core";
-import { ActivatedRoute, ParamMap } from "@angular/router";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 import { RatioDetailsService } from "src/app/services/ratio-details.service";
 import { AnalyzedData } from "src/app/shared/sharedTS";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
@@ -16,22 +16,21 @@ export class RatioDetailsDialogComponent implements OnInit {
     ratiosInfos: AnalyzedData[];
     isLoading: boolean = true;
     ratioName: string;
+    testBackendDataDisplay:string;
 
-    constructor(public route: ActivatedRoute, public RatioDetailsService: RatioDetailsService,
-        public dialogRef: MatDialogRef<RatioDetailsDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: AnalyzedData /* TODO any only for experiments*/) {
-            this.isLoading = false;
+    constructor(public route: ActivatedRoute, private router: Router, public RatioDetailsService: RatioDetailsService,
+        public dialogRef: MatDialogRef<RatioDetailsDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: AnalyzedData) {
         if (data) {
             this.ratioName = data.name;
         }
     }
 
     ngOnInit() {
-        this.route.paramMap.subscribe((paramMap: ParamMap) => {
-            this.assetId = paramMap.get(`assetId`);
-            this.RatioDetailsService.getRatiosDetails(this.assetId).subscribe(ratiosDetailedInfos => {
-                this.isLoading = false;
-                this.ratiosInfos = ratiosDetailedInfos;
-            });
+        let assetId = this.router.url.split(`s/`)
+        this.RatioDetailsService.getRatiosDetails(assetId[1]).subscribe(ratiosDetailedInfos => {
+            this.isLoading = false;
+            this.ratiosInfos = ratiosDetailedInfos;
+            this.testBackendDataDisplay=this.ratiosInfos[0].shortly[0]
         })
     }
 }
