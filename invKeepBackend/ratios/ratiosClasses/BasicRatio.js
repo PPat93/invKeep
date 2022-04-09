1ee0c746519b294548abeb558c58ee5c4c2c2412
Piotr Paterek <ptrptrk@gmail.com>
2022-04-09 22:44:26 +0100

    v0.07.11 - add default values describing ratios variables in BasicRatio class, remove unnecessary constructor value set

--- invKeepBackend/ratios/ratiosClasses/BasicRatio.js
+++ invKeepBackend/ratios/ratiosClasses/BasicRatio.js
@@ -2,20 +2,23 @@ const SharedJS = require('../../../invKeepFrontend/src/app/shared/sharedJS');
 
 module.exports = class BasicRatio {
 
-    ratioName = ``;
-    coAnalysis = [];
-    shortDescription = ``;
-    extensiveDescription = ``;
-    formula = ``;
-    example = ``;
-    bulletPointSummary = [];
+    ratioName = `Default Name`;
+    coAnalysis = [`First default coanalysis ratio name`, `Second default coanalysis ratio name`];
+    shortDescription = `Default short ratio description about the ratio`;
+    extensiveDescription = `Default long, extensive ratio description that is going on and on and on. Details and more...`;
+    formula = `(default formula value) = a + b/2`;
+    example = `If ratio is being bigger and bigger while something, then something and it is defaultly worth/not worth to buy`;
+    bulletPointSummary = [
+        `First default ratio summary point`,
+        `Second default ratio summary point`,
+        `Third default ratio summary point`
+        ];
     finalValue = 0;
-    intervalsData = [{ name: `${this.ratioName}`, verbalRating: ``, summary: ``, numberRating: 0 }];
+    intervalsData = [{ name: `${this.ratioName}`, verbalRating: SharedJS.RatingObject.neutral, summary: `Default analysis ratio short comment`, numberRating: 0 }];
     intervals = [[-Infinity, 0], [0, Infinity]];
 
-    constructor(ratio = 0, msg = ``) {
+    constructor(ratio = 0) {
         this.finalValue = ratio;
-        this.shortDescription = msg;
     }
 
     determineProfitability(intervals = this.intervals, intervalsData = this.intervalsData) {
