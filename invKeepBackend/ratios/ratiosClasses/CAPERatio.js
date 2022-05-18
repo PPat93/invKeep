b827cc140763f4444d643cc7b753391e7a120a3e
Piotr Paterek <ptrptrk@gmail.com>
2022-05-18 23:13:12 +0100

    v0.07.12 - update CAPE example

--- invKeepBackend/ratios/ratiosClasses/CAPERatio.js
+++ invKeepBackend/ratios/ratiosClasses/CAPERatio.js
@@ -12,11 +12,17 @@ module.exports = class CAPERatio extends BasicRatio {
             recessions/expansions and makes easier to analyze company without them. The lower, the better. However, it does 
             not involve accounting changes.`;
         this.extensiveDescription = `${this.ratioName} measures real earnings per share value throughout 10 years. It is 
-        showing average earnings considering inflation. Thanks to a long period of time taken into account, ratio is resistant
-        to fluctuations of economic cycles. Company profit changes ocurring during recessions and high prosperity are adjusted 
-        with averaged inflation. That allow to present if stock price is undervalued or overvalued.`;
+            showing average earnings considering inflation. Thanks to a long period of time taken into account, ratio is resistant
+            to fluctuations of economic cycles. Company profit changes ocurring during recessions and high prosperity are adjusted 
+            with averaged inflation. That allow to present if stock price is undervalued or overvalued.`;
         this.formula = [`CAPEvalue`, `Stock Price`, `10 years Inflation Adjusted Earnings`];
-        this.example = ``;
+        this.example = `Let's say, that The Company share price is worth currently 1000$. Throughout the last 10 years Earning
+            per Share was fluctuating from 20$ - 100$ and the inflation was between 10% - 20% depending on year. To adjust inflation, 
+            each years EPS should be multiplied by inflation of that year, e.g.: 100$ * (1 - 0.2) = 80$. After that all results are
+            summed up and divided by 10 years - let's assume it is 58.12$. That is the 10 years Inflation Adjusted Earnings value. Last 
+            step is to divide Current Share price by the 10 years Inflation Adjusted Earnings value, so 1000$ / 58.12$ = 17.206. 
+            Value 17.206 indicates quite OK possibilities for the company on the basis of average returns of 10 - 15 years of American
+            companies.`;
         this.bulletPointSummary = [
             `Adjusted to economic cycles.`,
             `Shows if stock is over or under valued.`,
