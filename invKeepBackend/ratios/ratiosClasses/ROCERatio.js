137818d44078be2a41655fb78142c262614d7fa4
Piotr Paterek <ptrptrk@gmail.com>
2022-06-08 21:51:10 +0100

    v0.07.12 - update roce exvtensive description and exmaple variables with data

--- invKeepBackend/ratios/ratiosClasses/ROCERatio.js
+++ invKeepBackend/ratios/ratiosClasses/ROCERatio.js
@@ -9,12 +9,22 @@ module.exports = class ROCERatio extends BasicRatio {
         this.coAnalysis = [`ROA, ROE, ROIC`];
         this.shortDescription = `Return On Capital Employed Ratio allows for analysis of company profitability on its capital.
             It is especially useful for performance comparison of firms in capital-sensitive branch sectors, e.g. telecoms 
-            or utilities. Takes into account debt and equity. All in all it shows how many profit is generated per 1$. The 
-            the higher the value, the better profitability of the company is. Usually investor prefers companies with stable
-            ROCE instead of volatile one. Can be affected by high cash resources. In this app is presented in %`;
-        this.extensiveDescription = ``;
+            or utilities. Takes into account debt and equity. It shows how many profit is generated per $1. The the higher the 
+            value, the better profitability of the company is. Usually investor prefers companies with stable ROCE instead of 
+            volatile one. Can be affected by high cash resources. In this app is presented in %.`;
+        this.extensiveDescription = `${this.ratioName} takes into the account both debt and equity, thanks to which performance
+            of companies with a signifcant debt is properly adjusted because of it. This ratio is quite similar to ROIC Ratio, as
+            both are measuring profitability per capital of the business. Importantly, ${this.ratioName} is not considering 
+            company's tax liabilities, so it is not that vast. It is especially helpful when multiple similar companies are 
+            compared on different financial metrics. If the analysisis conducted with ROA, ROIC and ROE ratios, then values
+            are comprehensive and are complementing each other. However, ROE and ROA are better and more complete indicators 
+            that can asses a company on their own.`;
         this.formula = [this.ratioName, `(Net Income + Interest + Taxes) OR (Revenue - Costs of Goods Sold - Operating Expenses)`, `Capital Employed`];
-        this.example = ``;
+        this.example = `Let's assume that The Company 1 has: EBIT = $4 000 and Capital employed = $9 000. We also have 
+            The Company 2 that has: EBIT = $14 000, and Capital employed = $90 000. In those cases, ${this.ratioName} for TC1 
+            is 44% and for TC2 is 15%. So, despite TC2 looks like a bigger and better company, it is only generating 15 cents 
+            per capital dollar, while 'smaller' TC1 is creating 44 cents per capital dollar. If only this ratio would be taken
+            into the account, TC1 would be better choice than TC2.`;
         this.bulletPointSummary = [
             `The higher, the better.`,
             `Allows comparison between capital-sensitive industry companies.`,
