c73d7a6e8473ae138ee7da3f5af21afb0639eec4
Piotr Paterek <ptrptrk@gmail.com>
2022-06-11 22:26:56 +0100

    v0.07.13 - prepare to create Ratio Detail modal covering tests and remove unused fixture json file as it is not needed

---
+++ invKeepTests/cypress/integration/e2e/RatiosAnalysis/RatioDetailsModal.spec.ts
@@ -0,0 +1,30 @@
+import MainPage from "../../../support/pageObjectModel/pageObjects/MainPage";
+import Utils, { AssetCurrency } from "../../../support/pageObjectModel/Utils/Utils";
+import AnalysisPageConsts from "../../../support/pageObjectModel/Utils/AnalysisPageConsts";
+import AnalysisPage from "../../../support/pageObjectModel/pageObjects/AnalysisPage";
+
+describe(`Analysis Ratios Ratio Details Modal displayment`, () => {
+
+    let assetName: string = ``;
+
+    beforeEach(`Create test asset`, () => {
+
+        assetName = `TestAsset${Date.now()}`;
+
+
+        cy.apiCreateAsset(assetName, `TASbl`, 19, 245.5, AssetCurrency.euro);
+        Utils.visitPage(Utils.mainPageUrl);
+        cy.getDataCyElement(MainPage.dataCyElementAsset(assetName))
+            .click();
+        cy.getDataCyElement(MainPage.dataCyElementDetailsBtn(assetName))
+            .click();
+    });
+
+    afterEach(`Little teardown`, () => {
+        Utils.teardownAssets(`TestAsset`);
+    })
+
+    it(`Ratios Analysis - Ratio Details Modal displayment`, () => {
+
+    })
+})
