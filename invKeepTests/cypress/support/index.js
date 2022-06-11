07c63ef4acc901d685087a2349229c55f161b938
Piotr Paterek <ptrptrk@gmail.com>
2022-06-11 23:30:04 +0100

    v0.07.13 - add ratio details dialog div block and data-cy attriute + add adjusting viewport before all tests

--- invKeepTests/cypress/support/index.js
+++ invKeepTests/cypress/support/index.js
@@ -3,7 +3,10 @@ import Utils from './pageObjectModel/Utils/Utils';
 
 Cypress.env("assetItem", new Map())
 
+before(`Change viewport`, () => {
+    cy.viewport(1200,800);
+})
+
 after(`Total teardown`, () => {
-    
     Utils.teardownAssets(`TestAsset`);
 })
