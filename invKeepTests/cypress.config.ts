b03d0252a3ac109c673d9090be7d0d3691dfd4dd
Piotr Paterek <ptrptrk@gmail.com>
2022-07-06 23:10:38 +0100

    v0.07.13.7 - start Cypress 10 migration

---
+++ invKeepTests/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "https://localhost:4200",
+    supportFile: "cypress/support/index.js",
+    env: {
+        backendUrl: "http://localhost:3000/api/assets"
+    }
+  }
+})
