b73350c0ab97b4b8586c4f207bf14d6b5c546c60
Piotr Paterek <ptrptrk@gmail.com>
2022-07-09 01:07:37 +0100

    v0.07.13.7 - further cypress 10 migration, change local config file format to be ts and update object export so it is no longer json and can be used in setupnode events functions. Temporarily tested with local only cinfig loading, iterating through object and on return compare and update new config

--- invKeepTests/cypress.config.ts
+++ invKeepTests/cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'cypress'
+import configLocal from './cypress/config/local'
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200",
-    supportFile: "cypress/support/index.js",
-    env: {
-        backendUrl: "http://localhost:3000/api/assets"
-    }
+    setupNodeEvents(on, config) {
+      Object.entries(configLocal).forEach(([key, value]) => {
+        config[key] = value;
+      });
+      return config
+    },
   }
 })
