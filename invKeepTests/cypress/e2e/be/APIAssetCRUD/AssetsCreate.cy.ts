7d04a93ee20ab88e304271c1adc8ec51258d02cb
Piotr <ptrptrk@gmail.com>
2023-07-15 23:59:23 +0100

    v0.08.06 - start defining api tests

--- invKeepTests/cypress/e2e/be/APIAssetCRUD/AssetsCreate.cy.ts
+++ invKeepTests/cypress/e2e/be/APIAssetCRUD/AssetsCreate.cy.ts
@@ -1,15 +1,32 @@
 
-describe(`API Assets creation`, () => {
-
-    beforeEach(`Visit main page`, () => {
-
-    });
+describe(`API - valid asset creation`, () => {
 
     afterEach(`Little teardown`, () => {
 
     })
 
-    it(`API - Create asset without purchase date`, () => {
+    it(`API - Create asset without purchase date`, () => { })
+    it(`API - Create asset with purchase date`, () => { })
+    it(`API - Create asset with maximum value lengths`, () => { })
+    it(`API - Create asset with minimum value lengths`, () => { })
+})
+
+describe(`API - attempt invalid asset creation`, () => {
+
+    afterEach(`Little teardown`, () => {
 
     })
+
+    it(`API - Attempt to create asset without any of the required fields`, () => { })
+    it(`API - Attempt to create asset with invalid datatype for Name field`, () => { })
+    it(`API - Attempt to create asset with invalid datatype for Symbol field`, () => { })
+    it(`API - Attempt to create asset with invalid datatype for Amount field`, () => { })
+    it(`API - Attempt to create asset with invalid datatype for Price field`, () => { })
+    it(`API - Attempt to create asset with invalid datatype for Currency field`, () => { })
+    it(`API - Attempt to create asset with invalid datatype for Purchase Date field`, () => { })
+    it(`API - Attempt to create asset with too long value for any of the fields`, () => { })
+    it(`API - Attempt to create asset with too short value for any of the fields`, () => { })
+    it(`API - Attempt to create asset with too short value for any of the fields`, () => { })
+    it(`API - Attempt to create asset with additional fields`, () => { })
+    it(`API - Attempt to set asset ID during asset creation`, () => { })
 })
