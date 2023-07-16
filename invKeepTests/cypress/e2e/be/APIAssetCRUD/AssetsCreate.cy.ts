da674bf80cf510e72ec269b547c3988f5bae4fc0
Piotr <ptrptrk@gmail.com>
2023-07-16 02:02:17 +0100

    v0.08.06 - finish asset create tests definition, create api delete asset suite and define a couple first ones

--- invKeepTests/cypress/e2e/be/APIAssetCRUD/AssetsCreate.cy.ts
+++ invKeepTests/cypress/e2e/be/APIAssetCRUD/AssetsCreate.cy.ts
@@ -5,10 +5,13 @@ describe(`API - valid asset creation`, () => {
 
     })
 
-    it(`API - Create asset without purchase date`, () => { })
-    it(`API - Create asset with purchase date`, () => { })
-    it(`API - Create asset with maximum value lengths`, () => { })
-    it(`API - Create asset with minimum value lengths`, () => { })
+    it(`API - Create an asset without purchase date`, () => { })
+    it(`API - Create an asset with purchase date`, () => { })
+    it(`API - Create an asset with maximum value lengths`, () => { })
+    it(`API - Create an asset with minimum value lengths`, () => { })
+    it(`API - Create an asset identical to another one`, () => { })
+    it(`API - Attempt to create an asset with additional fields`, () => { })
+    it(`API - Attempt to set asset ID during asset creation`, () => { })
 })
 
 describe(`API - attempt invalid asset creation`, () => {
@@ -17,16 +20,29 @@ describe(`API - attempt invalid asset creation`, () => {
 
     })
 
-    it(`API - Attempt to create asset without any of the required fields`, () => { })
-    it(`API - Attempt to create asset with invalid datatype for Name field`, () => { })
-    it(`API - Attempt to create asset with invalid datatype for Symbol field`, () => { })
-    it(`API - Attempt to create asset with invalid datatype for Amount field`, () => { })
-    it(`API - Attempt to create asset with invalid datatype for Price field`, () => { })
-    it(`API - Attempt to create asset with invalid datatype for Currency field`, () => { })
-    it(`API - Attempt to create asset with invalid datatype for Purchase Date field`, () => { })
-    it(`API - Attempt to create asset with too long value for any of the fields`, () => { })
-    it(`API - Attempt to create asset with too short value for any of the fields`, () => { })
-    it(`API - Attempt to create asset with too short value for any of the fields`, () => { })
-    it(`API - Attempt to create asset with additional fields`, () => { })
-    it(`API - Attempt to set asset ID during asset creation`, () => { })
+    it(`API - Attempt to create an asset without any of the required fields`, () => { })
+
+    it(`API - Attempt to create an asset with invalid datatype for Name field`, () => { })
+    it(`API - Attempt to create an asset with invalid value for Name field`, () => { })
+
+    it(`API - Attempt to create an asset with invalid datatype for Symbol field`, () => { })
+    it(`API - Attempt to create an asset with invalid value for Symbol field`, () => { })
+
+    it(`API - Attempt to create an asset with invalid datatype for Amount field`, () => { })
+    it(`API - Attempt to create an asset with invalid value for Amount field`, () => { })
+
+    it(`API - Attempt to create an asset with invalid datatype for Price field`, () => { })
+    it(`API - Attempt to create an asset with invalid value for Price field`, () => { })
+
+    it(`API - Attempt to create an asset with invalid datatype for Currency field`, () => { })
+    it(`API - Attempt to create an asset with invalid value for Currency field`, () => { })
+
+    it(`API - Attempt to create an asset with invalid datatype for Purchase Date field`, () => { })
+    it(`API - Attempt to create an asset with invalid value for Purchase Date field`, () => { })
+
+    it(`API - Attempt to create an asset with too long value for any of the fields`, () => { })
+
+    it(`API - Attempt to create an asset with too short value for any of the fields`, () => { })
+
+    it(`API - Attempt to create an asset with missing payload`, () => { })
 })
---
+++ invKeepTests/cypress/e2e/be/APIAssetCRUD/AssetsDelete.cy.ts
@@ -0,0 +1,19 @@
+
+describe(`API - valid asset deletion`, () => {
+
+    afterEach(`Little teardown`, () => {
+
+    })
+
+    it(`API - Delete an asset without purchase date`, () => { })
+    it(`API - Delete an asset with purchase date`, () => { })
+})
+
+describe(`API - attempt invalid asset deletion`, () => {
+
+    afterEach(`Little teardown`, () => {
+
+    })
+
+    it(`API - Delete a non-existing asset`, () => { })
+})
